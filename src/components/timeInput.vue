bdfb21bd01f7c1757b53231cd818359306ffbf8b
gderuwez <gderuwez@gmail.com>
2018-10-18 12:30:31 +0200

    first commit

---
+++ src/components/timeInput.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <form class="" @submit="testing" action="" method="post">
+    <div class=""  v-bind:key="item.id" v-for="item in timesInput">
+      <label v-bind:for="item.id">{{item.time}}</label>
+      <input type="time" v-bind:id="item.id" name="" value="00:00">
+    </div>
+    <button type="submit" name="button">submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "TimeInput",
+  methods: {
+    testing : function (e) {
+      e.preventDefault();
+      for (var i = 0; i < (e.target.length); i++) {
+        if (e.target[i].type === "time") {
+          console.log(e.target[i].value);
+        }
+      }
+    }
+  },
+  data: function () {
+    return {
+      breakfastTime: '',
+      lunchTime: '',
+      dinnerTime: '',
+      timesInput: [
+        {time:"breakfast", modelToUse: "breakfastTime", id:"breakfastId"},
+        {time:"lunch", modelToUse: "lunchTime", id:"lunchId"},
+        {time:"dinner", modelToUse: "dinnerTime", id:"dinnerId"},
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div class="">
+    <div v-if="check" class="">
+      <p >Cette application est un outil pour mieux se connaître. Elle ne remplace en aucun cas l'avis d'un professionel. En cas de doute, consultez votre medecin</p>
+      <p >Les données collectées par cette application ne sont pas storée en ligne, toute les données son présente uniquement sur votre appareil.</p>
+      <button type="button" name="button" v-on:click="checking">Don't remind me</button>
+      <hr />
+    </div>
+
+    <TimeInput />
+
+  </div>
+</template>
+
+<script>
+import TimeInput from '@/components/timeInput.vue'
+
+export default {
+  components: {
+    TimeInput
+  },
+  data: function () {
+    return {
+      check: true,
+      value: ''
+    }
+  },
+  mounted() {
+    if (localStorage.check) {
+      var turntoBool = true;
+      if (localStorage.check === "false") {
+        turntoBool = false;
+      }
+      this.check = turntoBool;
+    }
+    if (localStorage.value) {
+      this.value = localStorage.value
+    }
+  },
+  methods: {
+    checking() {
+      this.check = !this.check;
+      localStorage.check = this.check;
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .test {
+    color: red;
+  }
+</style>
